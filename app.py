d2b1f8130de7ffe803d0d1a20f91e9de2d58931a
Harshit Raizada <115217128+harshit-raizada@users.noreply.github.com>
2023-02-25 01:36:37 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.title(':red[Innomatics] :blue[Data] :green[App]')
+
+st.subheader('About Me:sunglasses:')
+
+st.text('I am Harshit Raizada.')
+st.text('I am a Data Science Intern at Innomatics Research Labs.')
+st.text('I am a pre-final year student pursuing a bachelors degree in Technology with a specialisation in Machine Learning & Artificial Intelligence.') 
+st.text('I have a keen interest in working within the Machine Learning domain. The programming language I have a strong hold on is Python.') 
+st.text('I am an initiative taker and demonstrate quick concept-grasping skills')
+
+btn = st.button('Connect with me on LinkedIn')
+if btn == True:
+    st.write('https://www.linkedin.com/in/harshit-raizada/')
+    st.balloons()
+
+btn1 = st.button('Link to GitHub')
+if btn1 == True:
+    st.write('https://github.com/harshit-raizada')
+    st.snow()
---
+++ pages/1_Dashboard_IPL.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from matplotlib import image
+import pandas as pd
+import plotly.express as px
+import os
+
+FILE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+PARENT_DIR = os.path.join(FILE_DIR, os.pardir)
+
+dir_of_interest = os.path.join(PARENT_DIR, "resources")
+
+IMAGE_PATH = os.path.join(dir_of_interest, "images", "ipl_logo.jpg")
+DATA_PATH = os.path.join(dir_of_interest, "data", "ipl.csv")
+
+st.title(":red[IPL] :green[Auction] :blue[2023] :fire:")
+
+img = image.imread(IMAGE_PATH)
+st.image(img)
+
+df = pd.read_csv(DATA_PATH)
+st.dataframe(df)
+
+team = st.selectbox("Select the Franchise:", df['Team'].unique())
+
+col1, col2, col3 = st.columns(3)
+
+fig_1 = px.histogram(df[df['Team'] == team], x="Price Cr")
+col1.plotly_chart(fig_1, use_container_width=True)
+
+fig_2 = px.box(df[df['Team'] == team], y="Price Cr")
+col2.plotly_chart(fig_2, use_container_width=True)
+
+fig_3 = px.scatter(df[df['Team'] == team], y="Price Cr")
+col3.plotly_chart(fig_3, use_container_width=True)
